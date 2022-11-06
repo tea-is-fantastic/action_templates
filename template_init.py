e2ddd81c40b392361e5de3d747cc91149acfe071
Tea Is Fantastic <tif@replyqa.com>
2022-11-07 00:54:23 +0500

    woohoo

--- template_init.py
+++ template_init.py
@@ -1,8 +1,11 @@
 import os
+import shutil
 
 from scripts.tif_env import TEMP_PATH
 
 
 def template_init():
     print('action_templates >> template_init >> chdir')
+    shutil.rmtree(TEMP_PATH, ignore_errors=True)
+    os.mkdir(TEMP_PATH)
     os.chdir(TEMP_PATH)
